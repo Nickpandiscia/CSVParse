76f3db9f417b65ecfa9c15e7496d42591a6f7555
Nickpandiscia <npandiscia1@gmail.com>
2023-05-10 11:41:50 -0400

    initial commit

---
+++ CSVParse/Program.cs
@@ -0,0 +1,42 @@
+﻿using LINQtoCSV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVParse
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			ReadCsvFile();
+			//Reading Csv. file.
+
+			Console.ReadKey();
+			//Obtains the next character or function key pressed by the user. The pressed key is displayed in the console window.
+		}
+
+		private static void ReadCsvFile()
+		{
+			var csvFileDescription = new CsvFileDescription
+			{
+				FirstLineHasColumnNames = true,
+				IgnoreUnknownColumns= true,
+				SeparatorChar= ',',
+				UseFieldIndexForReadingData = true,
+			};
+			//Creating a "style" for the file.
+
+			var csvContext = new CsvContext();
+			var cryptos = csvContext.Read<Transactions>("crypto_transactions_record_20230331_133059.csv", csvFileDescription);
+
+			foreach(var transaction in cryptos )
+			{
+				Console.WriteLine($"{transaction.TimeStamp} | {transaction.TransactionDescription} | {transaction.Currency} | " +
+					$"{transaction.Amount} | {transaction.TransactionKind}");
+			}
+		}
+	}
+}
---
+++ CSVParse/Transactions.cs
@@ -0,0 +1,28 @@
+﻿using LINQtoCSV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVParse
+{
+	[Serializable]
+	public class Transactions
+	{
+		[CsvColumn(Name = "Timestamp" , FieldIndex = 1, OutputFormat = "dd-mm-yyyy hh:mm:ss")]
+		public DateTime TimeStamp { get; set; }
+
+		[CsvColumn(Name = "Transaction Description", FieldIndex = 2)]
+		public string TransactionDescription { get; set; }
+
+		[CsvColumn(Name = "Currency", FieldIndex = 3)]
+		public string Currency { get; set; }
+
+		[CsvColumn(Name = "Amount", FieldIndex = 4, OutputFormat = "C")]
+		public float Amount { get; set; }
+
+		[CsvColumn(Name = "Transaction Kind", FieldIndex = 5)]
+		public string TransactionKind { get; set; }
+	}
+}
